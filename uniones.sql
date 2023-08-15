995bd45356d5e2c6b40fee73ec6599769039d28f
Sebastián de los Ángeles <luissebastianangeles@gmail.com>
2023-08-15 08:20:19 -0300

    Nuevo archivo: ejemplos para trabajar uniones

--- uniones.sql
+++ uniones.sql
@@ -15,8 +15,9 @@ create table B(
 );
 
 create table R(
-    A_ID int unique not null,
-    B_ID int unique not null primary key    
+    ID int unique not null primary key auto_increment,
+    A_ID int not null,
+    B_ID int not null
 );
 
 alter table R
@@ -33,6 +34,66 @@ references B(ID)
 on update cascade
 on delete cascade;
 
+insert into A
+(dato)
+values
+("a1"),
+("a2"),
+("a3"),
+("a4"),
+("a5"),
+("a6");
+
+insert into B
+(dato)
+values
+("b1"),
+("b2"),
+("b3"),
+("b4"),
+("b5"),
+("b6"),
+("b7"),
+("b8"),
+("b9"),
+("b10"),
+("b11"),
+("b12");
+
+insert into R
+(A_ID, B_ID)
+values
+(2,4),
+(5,1),
+(6,1),
+(6,2),
+(2,5),
+(2,6),
+(4,2),
+(5,2),
+(1,2),
+(2,5),
+(1,5),
+(1,6),
+(4,6),
+(5,2),
+(3,2),
+(3,1),
+(2,5),
+(1,2),
+(6,2),
+(2,6),
+(2,5),
+(2,5),
+(6,4),
+(6,2),
+(6,2),
+(4,5),
+(6,1),
+(5,2),
+(1,4),
+(2,2),
+(5,2);
 
 
 
